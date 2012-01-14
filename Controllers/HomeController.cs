eb097923f7e0729de12b619c4f301d3deb7f3ebe
Eric Williams <ericwilliams76@gmail.com>
2012-01-14 00:51:46 -0700

    demo checkin

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MapR.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
